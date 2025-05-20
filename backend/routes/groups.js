63e0cd079be3e121e6136ce981d675fe52d2730e
PasovitTechnologies <tech.admin@eafo.info>
2025-05-20 16:43:54 +0530

    qr-backend updated

--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -17,7 +17,7 @@ async function getAccessToken(apiNameId, apiKey) {
     const response = await axios.post(
       'https://apiv2.speedexam.net/api/Token/Get-Access-Token',
       {
-        customerId: 945-54003745,
+        customerId: process.env.CUSTOMER_ID,
         apiNameId,
         apiKey,
       },
