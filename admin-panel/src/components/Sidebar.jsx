81ab9bc144571bcc5b54ed0570783a3ddac2316c
PasovitTechnologies <tech.admin@eafo.info>
2025-05-20 09:13:09 +0530

    qr-backend updated

--- admin-panel/src/components/Sidebar.jsx
+++ admin-panel/src/components/Sidebar.jsx
@@ -15,6 +15,7 @@ import {
 } from "react-icons/md";
 import { BiSolidOffer } from "react-icons/bi";
 import { IoBookSharp } from "react-icons/io5";
+import { GiNotebook } from "react-icons/gi";
 import { useTranslation } from "react-i18next";
 import "./Sidebar.css";
 
@@ -59,7 +60,7 @@ const Sidebar = ({ selectedOS, isSidebarOpen, setIsSidebarOpen }) => {
       { to: "/telegram", icon: <MdSend />, label: t("sidebar.telegram") },
       { to: "/enquiry", icon: <MdHelpOutline />, label: t("sidebar.enquiry") },
       { to: "/userbase", icon: <MdPeople />, label: t("sidebar.userbase") },
-      { to: "/preregister", icon: <MdPeople />, label: t("sidebar.preCourse") },
+      { to: "/preregister", icon: <GiNotebook />, label: t("sidebar.preCourse") },
       { to: "/notifications", icon: <MdNotificationsActive />, label: t("sidebar.notifications") },
       { to: "/coupon", icon: <BiSolidOffer />, label: t("sidebar.coupon") },
       { to: "/exams", icon: <IoBookSharp /> , label: t("sidebar.exam") },
